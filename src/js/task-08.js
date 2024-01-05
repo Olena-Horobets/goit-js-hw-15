b06102702480dd75d21e77a38962e0a4e137011a
Olena-Horobets <80359219+Olena-Horobets@users.noreply.github.com>
2024-01-05 20:06:31 -0330

    task 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,18 @@
+const form = document.querySelector(".login-form");
+const emailInput = document.querySelector('[name="email"]');
+const passwordInput = document.querySelector('[name="passwordInput"]');
 
+const onSubmit = (e) => {
+	e.preventDefault();
+	const email = form.elements.email.value;
+	const password = form.elements.password.value;
+
+	if (!email) alert("Please, enter your email");
+	else if (!password) alert("Please, enter your password");
+	else {
+		console.log({ email, password });
+		form.reset();
+	}
+};
+
+form.addEventListener("submit", onSubmit);
