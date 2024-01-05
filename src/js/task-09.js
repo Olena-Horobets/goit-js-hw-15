527f98bfaef302128d30f8842c15b4dd92340c81
Olena-Horobets <80359219+Olena-Horobets@users.noreply.github.com>
2024-01-05 20:15:30 -0330

    task 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,5 +1,18 @@
+const body = document.querySelector("body");
+const text = document.querySelector(".color");
+const btn = document.querySelector(".change-color");
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+	return `#${Math.floor(Math.random() * 16777215)
+		.toString(16)
+		.padStart(6, 0)}`;
 }
+
+function onClick(e) {
+	const color = getRandomHexColor();
+
+	body.style.backgroundColor = color;
+	text.innerText = color;
+}
+
+btn.addEventListener("click", onClick);
